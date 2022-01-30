2fcbdc925bb2c8cc4bc3288ce5a31bc5224a2c9b
Thyago <thyagopiske@gmail.com>
2022-01-30 04:35:42 -0300

    fix: commentary error

--- src/rook.cpp
+++ src/rook.cpp
@@ -17,7 +17,7 @@ bool Rook::isValidMove(std::string final_position, const Square board[][8]){
 std::vector<std::string> Rook::getPossibleMoves(const Square board[][8]){
 
     /**Transforms char 'a' into int 0, char 'b' into int 1... etc
-    in order to represent the lines as numbers between 0 and 7 instead of chars between a and h.**/
+    in order to represent the columns as numbers between 0 and 7 instead of chars between a and h.**/
     int piece_column = (int) getCurrentPosition()[0] - 'a';
 
     int piece_line = (int) getCurrentPosition()[1] - '1';
