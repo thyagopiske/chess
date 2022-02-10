06c6a94af392aa70ab45fbd52d6dcac5475facf4
Thyago <thyagopiske@gmail.com>
2022-02-10 14:27:39 -0300

    write game file feature added

--- main.cpp
+++ main.cpp
@@ -7,25 +7,26 @@
 
 int main()
 {
-    int option;
+
+    int menu_option;
     do
     {
-
         displayMenu();
-        std::cin >> option;
+        std::cin >> menu_option;
         cleanBuffer();
 
-        if(option < 1 || option > 3)
+        if(menu_option < 1 || menu_option > 3)
         {
             std::cout << "Invalid option! Please enter a number between 1 and 3\n";
             continue;
         }
 
-        switch(option)
+        switch(menu_option)
         {
             case 1:
             {
                 Board game_board;
+                std::vector<std::string> game_moves;
 
                 while(true)
                 {
@@ -34,6 +35,7 @@ int main()
 
                     if(game_board.isKingInCheck()){
                         if(game_board.isCheckmate()){
+                            game_moves.push_back(game_board.getWhosTurn() == "white"? "0-1" : "1-0");
                             std::cout << "Checkmate! "
                                         << game_board.getWhosTurn()
                                         << " lost!"
@@ -53,11 +55,52 @@ int main()
                     if(player_move == "q") break;
 
                     game_board.movePiece(player_move);
+                    game_moves.push_back(player_move);
 
                     game_board.switchTurn();
 
 
                 }
+
+                char option;
+                std::cout << "Do you want to save the game in a file? (y/n)\n";
+                std::cin >> option;
+                cleanBuffer();
+                while(option != 'y' && option != 'n'){
+                    std::cout << "Please enter 'y' to yes or 'n' to no: ";
+                    std::cin >> option;
+                    cleanBuffer();
+                }
+
+                if(option == 'y'){
+                    std::string file_path = "games/";
+                    std::string file_name;
+                    std::ofstream game_file;
+
+                    std::cout << "Enter the file name (without extension): ";
+                    std::getline(std::cin, file_name);
+
+                    file_path.append(file_name);
+                    file_path.append(".txt");
+
+                    game_file.open(file_path);
+                    if(game_file.is_open()){
+                        for(auto& player_move : game_moves)
+                            game_file << player_move << std::endl;
+
+                        game_file.close();
+                    }
+                    else{
+                        std::cout << "Could not open the file!\n";
+                        std::cout << "Enter any key to go back to main menu: ";
+                        std::cin >> option;
+                        break;
+                    }
+
+                }
+
+
+
             }
                 break;
             case 2:
@@ -135,7 +178,7 @@ int main()
                 break;
         }
 
-    }while(option != 3);
+    }while(menu_option != 3);
 
     return 0;
 }
